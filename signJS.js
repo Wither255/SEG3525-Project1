2d847798ac387f6a7dd7f6cc4b61ca12ea93dc5e
Louis Marleau <louis.marleau@rogers.com>
2025-02-09 17:03:33 -0500

    Sign up and Sign In pages

---
+++ signJS.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    document.getElementById("signup-link").addEventListener("click", function () {
+        window.location.href = "signUp.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+	
+	document.getElementById("signin-link").addEventListener("click", function () {
+        window.location.href = "signIn.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+	
+	document.getElementById("close-btn").addEventListener("click", function () {
+        window.location.href = "main.html"; 
+    })
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+	document.getElementById("dummyBack").addEventListener("click", function () {
+        window.location.href = "main.html"; 
+    })
+});
