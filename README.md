4d00367bb37660d3f4ff524f320876657b3f28f5
Louis Marleau <louis.marleau@rogers.com>
2025-02-10 22:38:22 -0500

    Added Catalog, Cart, Contact, Custom Lures and locate pages

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ Website UI
 
 
 Links for borrowed images:
-
+https://rawwfishing.com/
+https://fishmaster.com/blog/post/fish-in-lake
+https://whitehorsecountrypark.co.uk/fishing-banner/
+https://xpressboats.com/xb-banner-h18-zeke-fishing/
--- signJS.js
+++ signJS.js
@@ -1,28 +1,67 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-
-    document.getElementById("signup-link").addEventListener("click", function () {
+    document.getElementById("signup-link")?.addEventListener("click", function () {
         window.location.href = "signUp.html";
     });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-	
-	document.getElementById("signin-link").addEventListener("click", function () {
+		
+	document.getElementById("signin-link")?.addEventListener("click", function () {
         window.location.href = "signIn.html"; 
     });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
 	
-	document.getElementById("close-btn").addEventListener("click", function () {
+	document.getElementById("close-btn")?.addEventListener("click", function () {
         window.location.href = "main.html"; 
-    })
+	});
+	
+	document.getElementById("homeClick")?.addEventListener("click", function () {
+        window.location.href = "main.html"; 
+    });
+	
+	document.getElementById("customLures")?.addEventListener("click", function() {
+		window.location.href = "CustomLures.html";
+	});
+	
+	document.getElementById("contact-link")?.addEventListener("click", function() {
+		window.location.href = "Contact.html";
+	});
+	
+	document.getElementById("locate-link")?.addEventListener("click", function() {
+		window.location.href = "Locate.html";
+	});
+	
+	document.getElementById("cart-link")?.addEventListener("click", function() {
+		window.location.href = "Cart.html";
+	});
+	
+	document.getElementById("dummyBack")?.addEventListener("click", function () {
+        window.location.href = "main.html"; 
+    });
+	
 });
 
-document.addEventListener("DOMContentLoaded", function () {
+document.querySelectorAll('.dropdown-item').forEach(item => {
+        item.addEventListener('click', function() {
+            document.getElementById('dropdownMenuButton').textContent = this.textContent;
+        });
+    });
+	
+document.querySelectorAll('.dropdown-item').forEach(item => {
+        item.addEventListener('click', function() {
+            document.getElementById('dropdownMenuButton').textContent = this.textContent;
+        });
+    });
 
-	document.getElementById("dummyBack").addEventListener("click", function () {
-        window.location.href = "main.html"; 
-    })
-});
+ document.getElementById('submitButton').addEventListener('click', function() {
+
+        document.getElementById('successAlert').style.display = "block";
+
+        document.getElementById('dropdownMenuButton').textContent = "Options";
+
+        document.getElementById('nameInput').value = "";
+		document.getElementById('emailInput').value = "";
+
+        document.getElementById('slider1').value = document.getElementById('slider1').defaultValue;
+        document.getElementById('slider2').value = document.getElementById('slider2').defaultValue;
+
+        document.getElementById('optionCheckbox1').checked = false;
+        document.getElementById('optionCheckbox2').checked = false;
+ });
